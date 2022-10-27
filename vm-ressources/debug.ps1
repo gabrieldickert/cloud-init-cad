95c27abb9fcfa83e3640bfb108a4278b64865c3c
Gabriel Dickert <gabriel.dickert@informatik.hs-fulda.de>
2022-10-27 14:05:57 +0200

    test for user

--- vm-ressources/debug.ps1
+++ vm-ressources/debug.ps1
@@ -1,52 +1,7 @@
-# Download Python
-Invoke-WebRequest -Uri "https://www.python.org/ftp/python/3.7.4/python-3.7.4-amd64.exe" -OutFile "python-3.7.4-amd64.exe"
-# Install Python for all users
-.\python-3.7.4-amd64.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0 | Out-Null
-# Load Enviroment Varibales
-$env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
-$env:Path = [System.Environment]::ExpandEnvironmentVariables([System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User"))
-#download rhino
-Invoke-WebRequest -Uri "https://files.mcneel.com/dujour/exe/20220912/rhino_de-de_7.22.22255.05001.exe" -OutFile "rhino.exe"
-Write-Host "Before Install Rhino."| Out-File -FilePath .\log.txt
-#Licening how to automate? => ZOO_Server #rhino.exe -package -quiet -norestart -passive LICENSE_METHOD=STANDALONE LICENSE_KEY=RH70-R6GK-5GT0-3R0A-8R8L-1476 INSTALL_DE=1
-./rhino.exe -package -quiet -norestart -passive LICENSE_METHOD=ZOO ZOO_SERVER=10.0.20.6 INSTALL_DE=1 SEND_STATISTICS=0 | Out-Null
-#Inital Start of Rhino with EditPythonScript to ensure python works
-Write-Host "After Install Rhino."| Out-File -FilePath -Append.\log.txt
-$Process = [Diagnostics.Process]::Start("Rhino.exe ", "/nosplash /runscript=""_-EditPythonScript Debugging=On _Enter _Enter""")          
-Write-Host "After init Start"| Out-File -FilePath -Append .\log.txt
-Start-Sleep -Seconds 30   
-$id = $Process.Id                        
-try {            
-    Stop-Process -Id $id -ErrorAction stop            
-    Write-Host "Successfully killed the process with ID: $ID"           
-    #Download REST-Plugin
-    $restPluginName = "restservice.rhi"
-    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/gabrieldickert/cloud-init-cad/main/vm-ressources/rhinorestservice.rhi" -OutFile $restPluginName
-    #Download current used Rhino Modul Plugin
-    $modulPluginName = "rhinogears.rhi"
-    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/gabrieldickert/cloud-init-cad/main/vm-ressources/rhinogears.rhi" -OutFile $modulPluginName
-    #Install both Plugins
-    $currentDir = (Get-Item .).FullName#pwd
-    Write-Host "Before Install rhi plugins"| Out-File -FilePath  -Append .\log.txt
-    Start-Process -FilePath "C:\Program Files\McNeel\Rhino Installer Engine\x64\rhiexec.exe" -ArgumentList  "$($currentDir) - $($restPluginName)", "/admin /silent" -Wait
-    Start-Process -FilePath "C:\Program Files\McNeel\Rhino Installer Engine\x64\rhiexec.exe" -ArgumentList  "$($currentDir) - $($modulPluginName)", "/admin /silent" -Wait
-    Write-Host "After Install rhi plugins"| Out-File -FilePath -Append .\log.txt
-    #Add Firewall Rule to Match Azure Rule
-    netsh advfirewall firewall add rule name="InBoundRule1024" dir=in action=allow protocol=TCP localport=1024
-    #Set Port Forwading Rules (Every Request on port 1024 gets forwarded to port 8081)
-    netsh interface portproxy add v4tov4 listenport=1024 listenaddress=0.0.0.0 connectport=8081 connectaddress=127.0.0.1
-    #Download Server Code
-    $serverFileName = "server.py"
-    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/gabrieldickert/cloud-init-cad/main/vm-ressources/server.py" -OutFile $serverFileName
-    #Install Requirements for Server
-    pip install requests
-    pip install psutil
-    #Start Server in new window
-    start-process  powershell.exe -ArgumentList "-noExit", "-command", "python server.py"
-    #Hopefully Working!
-    exit 0 
-}
-catch {            
-    Write-Host "Failed to kill the process"            
-}
+$username = 'cad-master'
+$password = '!cadmaster1337'
 
+$securePassword = ConvertTo-SecureString $password -AsPlainText -Force
+$credential = New-Object System.Management.Automation.PSCredential $username, $securePassword
+Start-Process Notepad.exe -Credential $credential
+exit 0
