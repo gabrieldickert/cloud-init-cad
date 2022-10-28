29952bebc30bb313d6519166ca44030fc154d655
Gabriel Dickert <gabriel.dickert@informatik.hs-fulda.de>
2022-10-28 09:54:46 +0200

    added solme debug to rhino inside

--- vm-ressources/inside.py
+++ vm-ressources/inside.py
@@ -1,11 +1,13 @@
 import rhinoinside
-rhinoinside.load()
+path = rhinoinside.load(r"C:\Program Files\Rhino 7\System")
 import Rhino
 import os
 doc = Rhino.RhinoDoc.CreateHeadless(None)
+print(doc)
 Rhino.RhinoDoc.ActiveDoc = doc
 the_doc = Rhino.RhinoDoc.ActiveDoc
 the_doc.Objects.AddPoint(Rhino.Geometry.Point3d(1.0, 2.0, 3.0))
-cwd = os.getcwd();
-the_doc.WriteFile(cwd+'\\123est.3dm', Rhino.FileIO.FileWriteOptions())
-the_doc.Dispose()
+print(os.getcwd())
+#cwd = os.getcwd();
+#the_doc.WriteFile(cwd+'\\123est.3dm', Rhino.FileIO.FileWriteOptions())
+#the_doc.Dispose()
