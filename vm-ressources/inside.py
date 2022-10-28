004ed865c31aa12d65e1c2992eb65311da07800c
Gabriel Dickert <gabriel.dickert@informatik.hs-fulda.de>
2022-10-28 10:57:14 +0200

    added 3dm file creation agian for test

--- vm-ressources/inside.py
+++ vm-ressources/inside.py
@@ -8,6 +8,6 @@ Rhino.RhinoDoc.ActiveDoc = doc
 the_doc = Rhino.RhinoDoc.ActiveDoc
 the_doc.Objects.AddPoint(Rhino.Geometry.Point3d(1.0, 2.0, 3.0))
 print(os.getcwd())
-#cwd = os.getcwd();
-#the_doc.WriteFile(cwd+'\\123est.3dm', Rhino.FileIO.FileWriteOptions())
-#the_doc.Dispose()
+cwd = os.getcwd();
+the_doc.WriteFile(cwd+'\\123est.3dm', Rhino.FileIO.FileWriteOptions())
+the_doc.Dispose()
