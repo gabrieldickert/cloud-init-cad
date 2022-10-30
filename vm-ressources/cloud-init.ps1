3a68fd6236578cf45cc7d1b60ae66a62c4c72fc6
Gabriel Dickert <gabriel.dickert@informatik.hs-fulda.de>
2022-10-30 14:36:55 +0100

    testfile for process spawning

--- vm-ressources/cloud-init.ps1
+++ vm-ressources/cloud-init.ps1
@@ -1,16 +1,16 @@
-﻿#download rhino
+﻿# Download Python
+Invoke-WebRequest -Uri "https://www.python.org/ftp/python/3.7.4/python-3.7.4-amd64.exe" -OutFile "python-3.7.4-amd64.exe"
+# Install Python for all users
+.\python-3.7.4-amd64.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0 | Out-Null
+# Load Enviroment Varibales
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
+$env:Path = [System.Environment]::ExpandEnvironmentVariables([System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User"))
+#download rhino
 Invoke-WebRequest -Uri "https://files.mcneel.com/dujour/exe/20221009/rhino_de-de_7.23.22282.13001.exe" -OutFile "rhino.exe"
 #Licening how to automate? => ZOO_Server #rhino.exe -package -quiet -norestart -passive LICENSE_METHOD=STANDALONE LICENSE_KEY=RH70-R6GK-5GT0-3R0A-8R8L-1476 INSTALL_DE=1
 ./rhino.exe -package -quiet -norestart -passive LICENSE_METHOD=ZOO ZOO_SERVER=10.0.20.6 INSTALL_DE=1 SEND_STATISTICS=0 | Out-Null
-#Inital Start of Rhino with EditPythonScript to ensure python works
-invoke-expression 'cmd /c start powershell -NoExit -Command { 
-$AdminDomain=[System.Net.Dns]::GetHostByName($env:computerName).HostName
-$AdminUserName = "cad-master"
-$AdminPassword = "!cadmaster1337"
-Enable-PSRemoting -Force -SkipNetworkProfileCheck
-$credential = New-Object System.Management.Automation.PSCredential @(($AdminDomain + "\" + $AdminUsername), (ConvertTo-SecureString -String $AdminPassword -AsPlainText -Force))
-Start-Process -FilePath "C:\Program Files\Rhino 7\System\Rhino.exe"
-Start-Process -FilePath "C:\Program Files\Rhino 7\System\Rhino.exe"                                 `                                                    `
-}';
-Start-Sleep -Seconds 30   
+#Download Server Code
+$serverFileName = "server.py"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/gabrieldickert/cloud-init-cad/main/vm-ressources/teststart.py" -OutFile $serverFileName
+start-process  powershell.exe -ArgumentList "-noExit", "-command", "python server.py"
 exit 0
